1510a7bdca78ce0197d80869ff55737318b69584
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-09-24 09:18:43 +0200

    Struktur

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,37 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name="HelloServlet",
+        urlPatterns = "/api"
+)
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello!");
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name= req.getParameter("text");
+        String password= req.getParameter("Password");
+
+        if (name.equals("bob") & password.equals("123")){
+            resp.sendRedirect("Yes.html");
+        }
+        else{
+            resp.sendRedirect("index.html");
+
+        }
+    }
+}
+
