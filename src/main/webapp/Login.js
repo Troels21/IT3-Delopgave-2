d20476dfd541557f09d65e8bccee5f142553dad6
juliefalsig <juliefv@me.com>
2021-10-26 10:48:27 +0200

    Vis og skjul kode på loginsiden

--- src/main/webapp/Login.js
+++ src/main/webapp/Login.js
@@ -1,1 +1,15 @@
 
+const visibilityListen = document.getElementById("visibilityListen")
+visibilityListen.addEventListener("click", toggleVisibiliy) //brug functionen hvis man trykker på iconet
+
+    function toggleVisibiliy() {
+        const passwordInput = document.getElementById("kode")
+        const icon = document.getElementById("icon")
+        if (passwordInput.type === "password") { //vis koden
+            passwordInput.type = "text"
+            icon.innerText = "visibility_off"
+        } else { //vis ikke koden
+            passwordInput.type = "password"
+            icon.innerText = "visibility"
+        }
+    }
