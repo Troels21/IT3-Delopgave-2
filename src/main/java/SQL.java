948b298536e9c4ec69dca82c9e19eb56bceef8d5
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-10-26 11:10:25 +0200

    ListeRepo

--- src/main/java/SQL.java
+++ src/main/java/SQL.java
@@ -1,3 +1,9 @@
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
 
 public class SQL {
@@ -13,17 +19,28 @@ public class SQL {
     endTime datetime unique,
     note VARCHAR(255));*/
 
-    private SQL() {}
+    private SQL() {
+    }
+
     static private final SQL sqlOBJ = new SQL();
+
     static public SQL getSqlOBJ() {
         return sqlOBJ;
     }
 
+    private final String SSHhost = "130.225.170.176:22022";
+    private final String SSHUser = "s205869";
+
     private final String url = "jdbc:mysql://localhost:3306/listedb";
-    private final String user = "root";
-    private final String password = "1234";
+    private final String DatabaseHost="127.0.0.1";
+    private final String DatabaseUser = "root";
+    private final String DatabasePassword = "faxe2021";
+    private final int DatabasePort = 3306;
+    private int forwardport;
+
     private Connection myConn;
     public Statement myStatement;
+    private Session session;
 
 
     public void makeConnectionSQL() throws SQLException {
@@ -32,7 +49,7 @@ public class SQL {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        myConn = DriverManager.getConnection(url, user, password);
+        myConn = DriverManager.getConnection(url, DatabaseUser, DatabasePassword);
         myStatement = myConn.createStatement();
     }
 
@@ -46,5 +63,22 @@ public class SQL {
         }
     }
 
+    public void tunnelSSH() {
+        JSch jSch = new JSch();
+
+        try {
+            FileInputStream fis = new FileInputStream("/src/main/resources/Key_openSSH");
+            jSch.addIdentity(fis.toString());
+            session = jSch.getSession(SSHUser,SSHhost);
+
+            int forwardport = session.setPortForwardingL(0,DatabaseHost,DatabasePort);
+
+        } catch (FileNotFoundException | JSchException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 
 }
