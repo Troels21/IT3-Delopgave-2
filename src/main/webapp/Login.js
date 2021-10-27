2515973f1cb13bfc25c352933c4b17e6f1d5d3bb
juliefalsig <juliefv@me.com>
2021-10-27 11:42:24 +0200

    Tjek kodeord og cpr-nummer

--- src/main/webapp/Login.js
+++ src/main/webapp/Login.js
@@ -1,4 +1,4 @@
-
+/* kode til at skjule og vise kodeordet */
 const visibilityListen = document.getElementById('visibilityListen')
 visibilityListen.addEventListener('click', togglevisibiliy) //brug functionen hvis man trykker på iconet
 
@@ -11,5 +11,26 @@ visibilityListen.addEventListener('click', togglevisibiliy) //brug functionen hv
         } else { //vis ikke koden
             passwordInput.type = "password"
             icon.innerText = "visibility"
+        }
+    }
+
+/* kode til at validere koden */
+    function validate(){
+        const username = document.getElementById("cpr").value;
+        const password = document.getElementById("kode").value;
+        if(username == ""){
+            alert("Indtast venligst et CPR-nummer")
+            return false
+        }
+        if(password == ""){
+            alert("Indtast venligst et kodeord")
+            return false
+         }
+        if( username == "123456-7890" && password == "Test123"){
+            window.location = "StartSide.html";
+            return false;
+        }
+        else{
+            alert("Login fejlede - indtast venligst et gyldigt CPR-nummer og kodeord")
         }
     }
