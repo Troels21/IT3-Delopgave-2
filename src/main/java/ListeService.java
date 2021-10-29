59f4d39a3ec4e4b5f18690372e30b3424beefa53
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-10-29 17:47:04 +0200

    Lavet Patient udfyld. Lavet SQL fecth af datahent. Lavet rettelser på popud Journal. igang med Udfyld skema.

--- src/main/java/ListeService.java
+++ src/main/java/ListeService.java
@@ -1,70 +1,53 @@
+import com.google.gson.Gson;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.io.*;
 import java.sql.SQLException;
 import java.util.List;
 
 @Path("liste")
 @Produces({MediaType.APPLICATION_XML})
-@Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_FORM_URLENCODED})
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_FORM_URLENCODED})
 public class ListeService {
     @GET
-    public List<Patient> getPatient() throws IOException, SQLException {
+    public List<Patient> getPatient() throws SQLException {
         //return ListeRepo.getInstance().getpatientList();
         //return new FileReader("patientlist");
         return ListeDB.getInstance().getpatientList();
     }
-
+    /*
     @POST
-    public String makePatient(Patient patient) throws IOException, SQLException {
+    public String makePatient(Patient patient) throws SQLException {
         ListeDB.getInstance().insertPatientSQL(patient);
-            /*ListeRepo.getInstance().create(patient);
+            //ListeRepo.getInstance().create(patient);
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             String xmlString = mapper.writeValueAsString(ListeRepo.getInstance().getpatientList());
             System.out.println(xmlString);
             FileWriter fw = new FileWriter("patientlist",true);
-            fw.write(xmlString);*/
+            fw.write(xmlString);
         return "added patient: " + patient.toString();
-    }
+    }*/
 
+    @Path("listeSQL")
     @POST
-    @Path("liste2")
-    public String makepatientSQL(String s) throws SQLException {
-
-        String[] opdelt1 = s.split("&");
-
+    public String makepatientSQL(@FormParam("cpr") String cpr, @FormParam("name") String name, @FormParam("timestart") String timestart, @FormParam("timeend") String timeend, @FormParam("note") String notat) throws SQLException {
         Patient patient = new Patient();
-        patient.setCpr(opdelt1[0].substring(4));
-        patient.setName(cleanupString(opdelt1[1].substring(5)));
-        patient.setTimestart(cleanupTime(opdelt1[2].substring(10)));
-        patient.setTimeend(cleanupTime(opdelt1[3].substring(8)));
-        patient.setNote(cleanupString(opdelt1[4].substring(5)));
+        patient.setCpr(cpr);
+        patient.setName(name);
+        patient.setTimestart(timestart);
+        patient.setTimeend(timeend);
+        patient.setNote(notat);
 
         ListeDB.getInstance().insertPatientSQL(patient);
         return "added patient: " + patient.toString();
     }
 
-    //Andreas Kig væk det her er ikke pænt, og det er du skal bare minimere metoden (den virker end of story).
-    public String cleanupTime(String s) {
-        int plus = s.indexOf("+");
-        int unicode = s.indexOf("%");
-        int weirdA = s.indexOf("A");
-        String Cleanup = "";
-        Cleanup = s.substring(0, plus);
-        Cleanup = Cleanup + " " + s.substring(plus + 1, unicode);
-        Cleanup = Cleanup + ":" + s.substring(weirdA + 1);
-        return Cleanup;
-    }
+    @Path("listeSQL")
+    @GET
+    public String selectFromTime(@QueryParam("from") String from, @QueryParam("to") String to) throws SQLException {
 
-    public String cleanupString(String s) {
-        if (s.indexOf("+") > 0) {
-            String[] cleanArray = s.split("\\+");
-            String CleanNote = cleanArray[0];
-            for (int i = 1; i < cleanArray.length; i++) {
-                CleanNote = CleanNote + " " + cleanArray[i];
-            }
+        String json = new Gson().toJson(ListeDB.getInstance().getpatientListeDateTime(from, to));
 
-            return CleanNote;
-        } else return s;
+        return json;
     }
 }
--- src/main/webapp/StartSide.js
+++ src/main/webapp/StartSide.js
@@ -1,106 +1,137 @@
+function fecthcall(fra, til) {
+    fetch("http://localhost:8080/IT3_Delopgave_2_war/data/liste/listeSQL?" + new URLSearchParams({
+        from: fra,
+        to: til,
+    }))
+        .then(resp => resp.json()).then(data => udfyldskema(data))
+}
+
+function udfyldskema(data) {
+    let autotider = document.getElementsByClassName("autotider");
+    let tider = "";
+    for (let i = 0; i <= data.length; i++) {
+        let time = data[i].timestart.substring(11);
+        let name = data[i].name;
+        let cpr = data[i].cpr;
+
+        let text = time + "        " + name + "," + cpr;
+        tider += '<div class="time">${text}</div>';
+    }
+    autotider.innerHTML = tider;
+}
 
 //Kalendar
 
+
 const date = new Date();
 date.setMonth(8)
-const months= [
- "Januar",
- "Febuar",
- "Marts",
- "April",
- "Maj",
- "Juni",
- "Juli",
- "August",
- "September",
- "Oktober",
- "November",
- "December",
+const months = [
+    "Januar",
+    "Febuar",
+    "Marts",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "December",
 ];
 let mymonth = months[date.getMonth()];
 const dates = document.querySelector(".dates");
-const lastdates = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+const lastdates = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-document.getElementById("actualmonth").innerText =mymonth;
+document.getElementById("actualmonth").innerText = mymonth;
 
-const firstdayindex = date.getDay()-1;
+const firstdayindex = date.getDay() - 1;
 
-const prevlastdates = new Date(date.getFullYear(),date.getMonth(),0).getDate()
-const nextdayindex = new Date(date.getFullYear(),date.getMonth()+1,0).getDay();
-const nextdays = 7-nextdayindex;
+const prevlastdates = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+const nextdayindex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+const nextdays = 7 - nextdayindex;
 
-let days="";
+let days = "";
 
-for(let x=firstdayindex; x>0; x--){
- days+=`<div class="lastdates">${prevlastdates-x+1}</div>`;
+for (let x = firstdayindex; x > 0; x--) {
+    days += `<div class="lastdates">${prevlastdates - x + 1}</div>`;
 }
 
-for(let z=1;z<=lastdates;z++){
- days+= `<div>${z}</div>`;
+for (let z = 1; z <= lastdates; z++) {
+    days += `<div>${z}</div>`;
 
 }
-for(let y=1; y<=nextdays; y++){
- days+=`<div class="nextdates">${y}</div>`;
- dates.innerHTML=days;
+for (let y = 1; y <= nextdays; y++) {
+    days += `<div class="nextdates">${y}</div>`;
+    dates.innerHTML = days;
 }
-
 
 
 //Pop-up journal
 
- function openForm() {
-  document.getElementById("myForm").style.display = "block";
- }
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
 
- function closeForm() {
-  document.getElementById("myForm").style.display = "none";
- }
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    let frm = document.getElementsByClassName("form-container")[0];
+    frm.reset();
+}
 
- function noWeekend() {
-  let datetime = document.getElementById('datetime');
+function submitForm() {
+    document.getElementById("myForm").style.display = "none";
+    let frm = document.getElementsByClassName("form-container")[0];
+    frm.submit();
+    frm.reset();
+}
 
-  let day = new Date(datetime.value);
-  let endDay = new Date(datetime.value);
-  if (day.getDay() === 6 || day.getDay() === 0) {
-   alert('Weekends not allowed');
-   day.setDate(0)
-   datetime.value = "";
-  }
+function noWeekend() {
+    let datetime = document.getElementById('datetime');
 
-  let time = day.getMinutes();
+    let day = new Date(datetime.value);
+    let endDay = new Date(datetime.value);
+    if (day.getDay() === 6 || day.getDay() === 0) {
+        alert('Weekends not allowed');
+        day.setDate(0)
+        datetime.value = "";
+    }
 
-  if (time <= 8) {
-   day.setMinutes(0);
+    let time = day.getMinutes();
 
-   endDay.setMinutes(15);
-  }
-  if (time > 8 && time <= 23) {
-   day.setMinutes(15);
+    if (time <= 8) {
+        day.setMinutes(0);
 
-   endDay.setMinutes(30);
-  }
-  if (time > 23 && time <= 38) {
-   day.setMinutes(30);
+        endDay.setMinutes(15);
+    }
+    if (time > 8 && time <= 23) {
+        day.setMinutes(15);
 
-   endDay.setMinutes(45);
-  }
-  if (time > 38 && time <= 53) {
-   day.setMinutes(45);
+        endDay.setMinutes(30);
+    }
+    if (time > 23 && time <= 38) {
+        day.setMinutes(30);
 
-   endDay.setMinutes(0);
-   endDay.setHours(endDay.getHours() + 1);
-  }
-  if (time >= 53) {
-   day.setMinutes(0);
-   day.setHours(day.getHours() + 1);
+        endDay.setMinutes(45);
+    }
+    if (time > 38 && time <= 53) {
+        day.setMinutes(45);
 
-   endDay.setMinutes(15);
-   endDay.setHours(endDay.getHours() + 1);
-  }
+        endDay.setMinutes(0);
+        endDay.setHours(endDay.getHours() + 1);
+    }
+    if (time >= 53) {
+        day.setMinutes(0);
+        day.setHours(day.getHours() + 1);
 
-  let start = document.getElementById('timeStart');
-  let end = document.getElementById('timeEnd')
+        endDay.setMinutes(15);
+        endDay.setHours(endDay.getHours() + 1);
+    }
 
-  start.value = (day.getFullYear() + "-" + day.getMonth() + "-" + day.getDay() + " " + day.getHours() + ":" + day.getMinutes());
-  end.value = (endDay.getFullYear() + "-" + endDay.getMonth() + "-" + endDay.getDay() + " " + endDay.getHours() + ":" + endDay.getMinutes());
- }
+    let start = document.getElementById('timeStart');
+    let end = document.getElementById('timeEnd')
+    let timefree = document.getElementById("timefree")
+    start.value = (day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDay() + " " + day.getHours() + ":" + day.getMinutes());
+    end.value = (endDay.getFullYear() + "-" + (endDay.getMonth() + 1) + "-" + endDay.getDay() + " " + endDay.getHours() + ":" + endDay.getMinutes());
+    timefree.value = (day.getHours() + ":" + day.getMinutes() + " til " + endDay.getHours() + ":" + endDay.getMinutes() + "    d." + day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDay())
+}
